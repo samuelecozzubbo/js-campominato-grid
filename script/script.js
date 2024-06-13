e31c11897054100aae30ac2de118bfe518525d1e
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-13 15:24:43 +0200

    Feat: add basic css and create grid at button click

---
+++ script/script.js
@@ -0,0 +1,41 @@
+//seleziono il contenitore ed il bottone
+const container = document.getElementById("grid");
+const myBtn = document.querySelector("button");
+
+// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+myBtn.addEventListener("click",generateGrid);
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+
+
+
+
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+/*************** 
+    FUNZIONI
+ *******************/ 
+
+//Funzione Grid Generator
+function generateGrid() {
+    //aggiungo i blocchi per 64 volte
+    for(let i = 0; i < 100; i++) {
+    //creo uno square
+    const nuovobox = createElementWithClass("div", "square");
+    //aggiungo il blocco al contenitore
+    container.append(nuovobox);
+    
+    }   
+}
+
+
+//Funzione che crea un elemento html
+function createElementWithClass(tag, classToAdd) {
+    //creo l'elemento 
+    const element = document.createElement(tag);
+    //aggiungo la classe principale
+    element.classList.add(classToAdd);
+
+    return element;
+}
